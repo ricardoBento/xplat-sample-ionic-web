d2a9997e2306cf6f48c524747e2c40e9e2d7ea17
ricardoBento <ricardo@flowhospitalitytraining.co.uk>
2020-06-10 14:05:03 +0100

    commit

--- apps/web-myapp/src/app/features/home/components/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-import { BaseComponent } from '@testing/core';
-
-@Component({
-  selector: 'abc-home',
-  templateUrl: 'home.component.html'
-})
-export class HomeComponent extends BaseComponent {}
---
+++ apps/web-test/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'web-test',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/web-test/'
+};
